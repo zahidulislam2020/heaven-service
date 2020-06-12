2fa121b8fd6e05b0ba34c25f779198ffd03c1ce0
zahidulislam2020 <61370815+zahidulislam2020@users.noreply.github.com>
2020-06-12 19:39:02 +0300

    heaven-service

--- js/toggle-menu-script.js
+++ js/toggle-menu-script.js
@@ -5,7 +5,9 @@ var menubar = document.querySelector('.menu-container');
 var toggle =  document.querySelector('.hamburger-container');
 var hamburgerIcon = document.querySelector('.fa-bars');
 var crossIcon = document.querySelector('.fa-times');
+let menulinks = document.querySelectorAll('.item-link');
 
+//Toggle Function
 
 toggle.addEventListener('click', toggleFunction);
 
@@ -13,8 +15,24 @@ function toggleFunction() {
     if(menubar.classList.toggle('menu-container-sd-appear')) {
         hamburgerIcon.style.display = 'none';
         crossIcon.style.display = 'inline-block';
-    }else{
+        
+    }else {
         hamburgerIcon.style.display = 'inline-block';
         crossIcon.style.display = 'none';
     }
 };
+
+    menulinks.forEach(item => {
+        item.addEventListener('click', event => {
+            menubar.classList.remove('menu-container-sd-appear');
+            hamburgerIcon.style.display = 'inline-block';
+            crossIcon.style.display = 'none';
+        })
+    })
+
+    // The below code is for a single elements action
+
+    // let menulinks = document.querySelector('.item-link'); 
+    // menulinks.addEventListener('click', event => { 
+    //     alert("Zahidul");
+    // });
