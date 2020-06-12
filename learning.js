adc0f9ae3de0761befbff6d076839496190b1545
zahidulislam2020 <61370815+zahidulislam2020@users.noreply.github.com>
2020-06-12 15:20:45 +0300

    heaven-service

---
+++ learning.js
@@ -0,0 +1,44 @@
+var name = "zahidul Islam";
+
+// function first() {
+//     var welcome = "hello ";
+//     second();
+//     console.log(welcome + name);
+// }
+
+// function second() {
+//     var welcome2 = "Hi ";
+//     third();
+//     console.log(welcome2 + name);
+// }
+
+// function third() {
+//     var welcome3 = "Hey! ";
+//     console.log(welcome3 + name);
+// }
+
+// first();
+
+function first() {
+    var welcome = "hello ";
+    second();
+    console.log(welcome + name);
+    
+}
+
+function second() {
+    var welcome2 = "Hi ";
+    third();
+    console.log(welcome2 + name);
+    
+}
+
+function third() {
+    var welcome3 = "Hey! ";
+    console.log(welcome3 + name);
+}
+
+first();
+
+
+
